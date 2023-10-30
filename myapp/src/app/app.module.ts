38b7439129e050b15bdccd1b4fa6958b7f0fe275
JesusLag <101838280+JesusLag@users.noreply.github.com>
2023-10-30 12:47:24 -0300

    Add files via upload

---
+++ myapp/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RegistrarServiciosPipe } from './registrar-servicios.pipe';
+
+@NgModule({
+  declarations: [AppComponent, RegistrarServiciosPipe],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
